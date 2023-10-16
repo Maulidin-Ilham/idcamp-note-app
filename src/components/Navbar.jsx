f70f00b153e524d7e27c9c5bfd9741f7a23229de
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-10-16 10:48:48 +0700

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 const Navbar = () => {
   return (
     <>
-      <div className="w-2/3  flex justify-between p-4">
-        <div>
+      <div className="w-2/3  flex justify-between p-4 ">
+        <div className="flex flex-row space-x-10 items-end justify-center ">
           <h1 className="text-xl font-semibold cursor-pointer ">Logo</h1>
-        </div>
-        <div>
           <h1 className="text-xl font-semibold cursor-pointer">Archieve</h1>
+        </div>
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            placeholder="Search here..."
+            className="px-3 py-2 w-[260px] border-2 border-black text-black"
+          />
         </div>
       </div>
     </>
